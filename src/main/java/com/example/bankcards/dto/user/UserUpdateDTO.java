a6453aff7995a71322ed8bc906520853195425dd
paul <kulebyakinpaul@yandex.ru>
2026-04-01 22:02:08 +0300

    Add dto for user update request

---
+++ src/main/java/com/example/bankcards/dto/user/UserUpdateDTO.java
@@ -0,0 +1,43 @@
+package com.example.bankcards.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO для обновления данных пользователя.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO {
+
+    /**
+     * Полное имя пользователя.
+     * Формат: "Фамилия Имя".
+     */
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    /**
+     * Email пользователя.
+     */
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    /**
+     * Пароль пользователя.
+     */
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    /**
+     * Роль пользователя.
+     * Допустимые значения: "USER", "ADMIN".
+     * По умолчанию — "USER".
+     */
+    private String role;
+}
