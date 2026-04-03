5f015d449f5b30cfbc9d1811791ddb58d75928e4
paul <kulebyakinpaul@yandex.ru>
2026-04-03 23:33:34 +0300

    Change @NotBlank to @NotNull in UserCreateDTO

--- src/main/java/com/example/bankcards/dto/user/UserCreateDTO.java
+++ src/main/java/com/example/bankcards/dto/user/UserCreateDTO.java
@@ -2,6 +2,7 @@ package com.example.bankcards.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +26,7 @@ public class UserCreateDTO {
     /**
      * Email пользователя.
      */
-    @NotBlank(message = "Email is required")
+    @NotNull(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
