1ca4cb452f1e94b75846f0b463034e4a5afa969a
paul <kulebyakinpaul@yandex.ru>
2026-04-03 20:18:50 +0300

    Add admin user controller implementation

--- src/main/java/com/example/bankcards/controller/admin/user/AdminUserController.java
+++ src/main/java/com/example/bankcards/controller/admin/user/AdminUserController.java
@@ -1,64 +1,80 @@
 package com.example.bankcards.controller.admin.user;
 
 import com.example.bankcards.dto.user.UserCreateDTO;
+import com.example.bankcards.dto.user.UserResponseDTO;
 import com.example.bankcards.dto.user.UserUpdateDTO;
+import com.example.bankcards.service.AdminUserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/admin/user")
+@RequiredArgsConstructor
 @Log4j2
 public class AdminUserController {
 
+    private final AdminUserService adminUserService;
+
+    // TODO add role validation
     @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody UserCreateDTO dto) {
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserCreateDTO dto) {
         log.info("Received request to create a new user with data : {}", dto.toString());
-        return ResponseEntity.ok().build();
+        UserResponseDTO newUser = adminUserService.createUser(dto);
+        return ResponseEntity.created(URI.create("/admin/user/" + newUser.getId())).body(newUser);
     }
 
     @GetMapping
-    public ResponseEntity<Void> getAllUsers() {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         log.info("Received request to get all users");
-        return ResponseEntity.ok().build();
+        List<UserResponseDTO> users = adminUserService.getAllUsers();
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping
-    public ResponseEntity<Void> getAllUsersWithPaging(@RequestParam int page, @RequestParam int size) {
-        log.info("Received request to get all users with page : {} and size : {}", page, size);
-        return getAllUsers();
-    }
-
-    @GetMapping
-    public ResponseEntity<Void> getAllUsersWithRole(@RequestParam String role) {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsersWithRole(@RequestParam String role) {
         log.info("Received request to get all users with role : {}", role);
-        return getAllUsers();
+        return adminUserService.getAllUsersWithRole(role);
     }
 
+    // TODO add paging + filter
     @GetMapping
-    public ResponseEntity<Void> getAllUsersWithRoleAndPaging(
-            @RequestParam int page, @RequestParam int size, @RequestParam String role) {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsersWithRoleAndPaging(
+            @RequestParam(defaultValue = "0") @Min(0) int page,
+            @RequestParam(defaultValue = "10") @Min(1) int size,
+            @RequestParam(required = false) String role) {
         log.info("Received request to get all users with page : {} and size : {} role : {}", page, size, role);
-        return getAllUsers();
+        return adminUserService.getAllUsersWithPagingAndRole(page, size, role);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Void> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable @Min(1) Long id) {
         log.info("Received request to get User with id : {}", id);
-        return ResponseEntity.ok().build();
+        UserResponseDTO user = adminUserService.getUserById(id);
+        return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUserById(@PathVariable @Min(1) Long id) {
         log.info("Received request to delete User with id : {}", id);
-        return ResponseEntity.ok().build();
+        adminUserService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateUserById(@PathVariable Long id, @RequestBody UserUpdateDTO dto) {
+    public ResponseEntity<UserResponseDTO> updateUserById(
+            @PathVariable @Min(1) Long id,
+            @Valid @RequestBody UserUpdateDTO dto) {
         log.info("Received request to update User with id : {}", id);
-        return ResponseEntity.ok().build();
+        UserResponseDTO user = adminUserService.updateUserById(id, dto);
+        return ResponseEntity.ok().body(user);
     }
 
 }
