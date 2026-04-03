1ec9b097b01105fbcbe5cb77ed9648141b62374e
paul <kulebyakinpaul@yandex.ru>
2026-04-03 23:35:05 +0300

    Add request param validation and json parse exception handlers

--- src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
@@ -2,9 +2,9 @@ package com.example.bankcards.exception;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
@@ -32,13 +32,38 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String, String>> handleRequestVariableValidationException(HandlerMethodValidationException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getParameterValidationResults().forEach(paramResult ->
+            paramResult.getResolvableErrors().forEach(error -> {
+                String paramName = paramResult.getMethodParameter().getParameterName();
+                String errorMsg = error.getDefaultMessage();
+                errors.put(paramName, errorMsg);
+            })
+        );
+
+        log.warn("Handler method validation failed: {}", errors);
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<Map<String, String>> handleJsonParseException(HttpMessageNotReadableException ex) {
+        log.warn(ex.getMessage(), ex);
+        Map<String, String> error = new HashMap<>();
+        error.put("message", "Malformed JSON request");
+        return ResponseEntity.badRequest().body(error);
+    }
+
     // TODO add entity not found exception
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Map<String,String>> handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
         Map<String,String> errors = new HashMap<>();
         errors.put("message", "Internal Server Error");
-        log.error(ex.getMessage(), ex);
         return ResponseEntity.internalServerError().body(errors);
     }
 }
