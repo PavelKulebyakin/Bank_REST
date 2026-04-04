c34251dfd2a48cc7154ac5607e7b44e4ea5d7d69
paul <kulebyakinpaul@yandex.ru>
2026-04-04 23:37:02 +0300

    Add transfer service implementation method placeholder

---
+++ src/main/java/com/example/bankcards/service/impl/TransferServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.service.impl;
+
+import com.example.bankcards.dto.transfer.TransferDTO;
+import com.example.bankcards.service.TransferService;
+import org.springframework.stereotype.Service;
+
+@Service
+class TransferServiceImpl implements TransferService {
+
+    @Override
+    public void processTransfer(TransferDTO dto) {}
+}
