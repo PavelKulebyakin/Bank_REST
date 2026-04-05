60c7206a579803e9aed2c27816a9c6242ffc99e7
paul <kulebyakinpaul@yandex.ru>
2026-04-06 00:19:22 +0300

    Add pagination and fix naming in admin user service

--- src/main/java/com/example/bankcards/service/impl/AdminUserServiceImpl.java
+++ src/main/java/com/example/bankcards/service/impl/AdminUserServiceImpl.java
@@ -1,15 +1,21 @@
 package com.example.bankcards.service.impl;
 
 import com.example.bankcards.dto.user.UserCreateDTO;
-import com.example.bankcards.dto.user.UserResponseDTO;
+import com.example.bankcards.dto.user.UserInfoResponseDTO;
 import com.example.bankcards.dto.user.UserUpdateDTO;
+import com.example.bankcards.entity.Role;
 import com.example.bankcards.entity.UserEntity;
+import com.example.bankcards.exception.custom.InvalidRoleException;
 import com.example.bankcards.exception.custom.ResourceAlreadyExistsException;
 import com.example.bankcards.exception.custom.ResourceNotFoundException;
 import com.example.bankcards.mapper.UserMapper;
 import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.service.AdminUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,20 +27,22 @@ public class AdminUserServiceImpl implements AdminUserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
-    public UserResponseDTO createUser(UserCreateDTO dto) {
+    public UserInfoResponseDTO createUser(UserCreateDTO dto) {
         if (userRepository.existsByEmail(dto.getEmail())) {
             throw new ResourceAlreadyExistsException("Email уже используется: "  + dto.getEmail());
         }
         UserEntity user = userMapper.toEntity(dto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         UserEntity savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
 
     @Override
-    public List<UserResponseDTO> getAllUsers() {
+    public List<UserInfoResponseDTO> getAllUsers() {
         return userRepository
                 .findAll()
                 .stream()
@@ -43,7 +51,7 @@ public class AdminUserServiceImpl implements AdminUserService {
     }
 
     @Override
-    public UserResponseDTO getUserById(Long id) {
+    public UserInfoResponseDTO getUserById(Long id) {
         UserEntity user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Пользователь с id: " + id + " не найден"));
         return userMapper.toDto(user);
@@ -59,7 +67,7 @@ public class AdminUserServiceImpl implements AdminUserService {
 
     @Override
     @Transactional
-    public UserResponseDTO updateUserById(Long id, UserUpdateDTO dto) {
+    public UserInfoResponseDTO updateUserById(Long id, UserUpdateDTO dto) {
         UserEntity existingUser = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Пользователь с id: " + id + " не найден"));
 
@@ -68,9 +76,6 @@ public class AdminUserServiceImpl implements AdminUserService {
         }
         if (dto.getEmail() != null && !dto.getEmail().isBlank()) {
             existingUser.setEmail(dto.getEmail());
-        }
-        if (dto.getPassword() != null && !dto.getPassword().isBlank()) {
-            existingUser.setPassword(dto.getPassword());
         }
         if (dto.getRole() != null) {
             existingUser.setRole(UserMapper.stringToRole(dto.getRole()));
@@ -81,9 +86,37 @@ public class AdminUserServiceImpl implements AdminUserService {
         return userMapper.toDto(updatedUser);
     }
 
-    // TODO add filters and pagination support
     @Override
-    public List<UserResponseDTO> getAllUsersWithPagingAndRole(int page, int size, String role) {
-        return getAllUsers();
+    public List<UserInfoResponseDTO> getAllUsersWithPagingAndRole(int page, int size, String role) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        if (role == null || role.isBlank()) {
+            return getAllUsersWithPaging(pageable);
+        } else {
+            return getUsersByRoleAndPaging(role, pageable);
+        }
+    }
+
+    // Метод без фильтра по роли
+    private List<UserInfoResponseDTO> getAllUsersWithPaging(Pageable pageable) {
+        Page<UserEntity> usersPage = userRepository.findAll(pageable);
+        return usersPage.stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    // Метод с фильтром по роли
+    private List<UserInfoResponseDTO> getUsersByRoleAndPaging(String role, Pageable pageable) {
+        Role userRole;
+        try {
+            userRole = UserMapper.stringToRole(role);
+        } catch (InvalidRoleException ex) {
+            throw new IllegalArgumentException("Неверная роль пользователя: " + role);
+        }
+
+        Page<UserEntity> usersPage = userRepository.findAllByRole(userRole, pageable);
+        return usersPage.stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 }
