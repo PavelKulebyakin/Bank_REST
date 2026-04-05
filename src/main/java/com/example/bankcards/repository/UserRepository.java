c43a4b50bf3588da8675712f7c5c4d45af800a1c
paul <kulebyakinpaul@yandex.ru>
2026-04-06 00:07:11 +0300

    Add methods to user repository

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.example.bankcards.repository;
 
+import com.example.bankcards.entity.Role;
 import com.example.bankcards.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Page<UserEntity> findAllByRole(Role userRole, Pageable pageable);
 }
