3d037724aaac1cf5c291caf920a46ad3d575b454
paul <kulebyakinpaul@yandex.ru>
2026-04-06 00:16:04 +0300

    Add validation and authentication to transfer controller

--- src/main/java/com/example/bankcards/controller/user/TransferController.java
+++ src/main/java/com/example/bankcards/controller/user/TransferController.java
@@ -1,10 +1,13 @@
 package com.example.bankcards.controller.user;
 
 import com.example.bankcards.dto.transfer.TransferDTO;
+import com.example.bankcards.security.CustomUserDetails;
 import com.example.bankcards.service.TransferService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,11 @@ public class TransferController {
     private final TransferService transferService;
 
     @PostMapping
-    public ResponseEntity<Void> transferBetweenCards(@RequestBody TransferDTO dto) {
+    public ResponseEntity<Void> transferBetweenCards(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestBody @Valid TransferDTO dto) {
         log.info("Received transfer request from {} to {} amount {}", dto.getFromId(), dto.getToId(), dto.getAmount());
-        transferService.processTransfer(dto);
+        transferService.processTransfer(user.getId(), dto);
         return ResponseEntity.ok().build();
     }
 }
