4c8857c53129b02578ca1630c4c99da76f195251
paul <kulebyakinpaul@yandex.ru>
2026-04-06 00:18:22 +0300

    Add user card service implementation

--- src/main/java/com/example/bankcards/service/impl/UserCardServiceImpl.java
+++ src/main/java/com/example/bankcards/service/impl/UserCardServiceImpl.java
@@ -1,50 +1,103 @@
 package com.example.bankcards.service.impl;
 
-import com.example.bankcards.dto.card.CardBalanceDTO;
-import com.example.bankcards.dto.card.CardResponseDTO;
+import com.example.bankcards.dto.card.CardBalanceResponseDTO;
+import com.example.bankcards.dto.card.CardInfoResponseDTO;
+import com.example.bankcards.entity.CardEntity;
+import com.example.bankcards.entity.CardStatus;
+import com.example.bankcards.exception.custom.ResourceNotFoundException;
+import com.example.bankcards.mapper.CardMapper;
+import com.example.bankcards.repository.CardRepository;
 import com.example.bankcards.service.UserCardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 class UserCardServiceImpl implements UserCardService {
 
+    private final CardRepository cardRepository;
+    private final CardMapper cardMapper;
+
     @Override
-    public List<CardResponseDTO> getAllCards() {
-        return List.of();
+    public List<CardInfoResponseDTO> getAllCards(Long userId) {
+        List<CardEntity> cards = cardRepository.findAllByUserId(userId);
+        return cards.stream()
+                .map(cardMapper::toDto)
+                .toList();
     }
 
     @Override
-    public List<CardResponseDTO> getAllCardsWithPaging(Integer page, Integer size) {
-        return List.of();
+    public CardInfoResponseDTO getAllCardsByPanLastDigits(Long userId, String panLast4) {
+        CardEntity card = cardRepository.findFirstByUserIdAndLast4Digits(userId, panLast4)
+                .orElseThrow(() -> new ResourceNotFoundException("Карта с последними 4 цифрами : " + panLast4 + " не найдена"));
+        return cardMapper.toDto(card);
+    }
+
+    // TODO add another way to get specific card
+    @Override
+    public CardInfoResponseDTO getCardsById(Long userId, Long cardId) {
+        CardEntity card = cardRepository.findByIdAndUserId(cardId, userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Карта с id : " + cardId + " не найдена"));
+        return cardMapper.toDto(card);
     }
 
     @Override
-    public List<CardResponseDTO> getAllCardsWithStatus(String status) {
-        return List.of();
+    public CardBalanceResponseDTO getCardBalance(Long userId, Long cardId) {
+        CardEntity card = cardRepository.findByIdAndUserId(cardId, userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Карта с id : " + cardId + " не найдена"));
+        return CardBalanceResponseDTO.builder()
+                .cardId(card.getId())
+                .balance(card.getBalance())
+                .build();
     }
 
     @Override
-    public List<CardResponseDTO> getAllCardsByPanLastDigits(String panLast4) {
-        return List.of();
+    @Transactional
+    public void blockCard(Long userId, Long cardId) {
+        CardEntity card = cardRepository.findByIdAndUserId(cardId, userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Карта с id : " + cardId + " не найдена"));
+        if (card.getStatus() == CardStatus.BLOCKED) {
+            throw new IllegalStateException("Карта уже заблокирована");
+        }
+        card.setStatus(CardStatus.BLOCKED);
+        cardRepository.save(card);
     }
 
     @Override
-    public List<CardResponseDTO> getAllCardsById(Long id) {
-        return List.of();
+    public List<CardInfoResponseDTO> getAllCardsWithStatusAndPaging(Long userId, String status, Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        if (status == null || status.isBlank()) {
+            return getAllCardsWithPaging(userId, pageable);
+        } else {
+            return getCardsByStatusAndPaging(userId, status, pageable);
+        }
     }
 
-    @Override
-    public CardBalanceDTO getCardBalance(Long id) {
-        return null;
+    private List<CardInfoResponseDTO> getAllCardsWithPaging(Long userId, Pageable pageable) {
+        Page<CardEntity> cardsPage = cardRepository.findAllByUserId(userId, pageable);
+        return cardsPage.stream()
+                .map(cardMapper::toDto)
+                .toList();
     }
 
-    @Override
-    public void blockCard(Long id) {}
+    private List<CardInfoResponseDTO> getCardsByStatusAndPaging(Long userId, String status, Pageable pageable) {
+        CardStatus cardStatus;
+        try {
+            cardStatus = CardStatus.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("Неправильный статус карты: " + status);
+        }
 
-    @Override
-    public List<CardResponseDTO> getAllCardsWithStatusAndPaging(String status, Integer page, Integer size) {
-        return List.of();
+        Page<CardEntity> cardsPage = cardRepository.findAllByUserIdAndStatus(userId, cardStatus, pageable);
+        return cardsPage.stream()
+                .map(cardMapper::toDto)
+                .toList();
     }
 }
