fb33d197c25ffdc3d2947585edd61cecb477fb97
paul <kulebyakinpaul@yandex.ru>
2026-04-06 00:15:20 +0300

    Add validation and authentication to user controller

--- src/main/java/com/example/bankcards/controller/user/UserCardController.java
+++ src/main/java/com/example/bankcards/controller/user/UserCardController.java
@@ -1,13 +1,14 @@
 package com.example.bankcards.controller.user;
 
-import com.example.bankcards.dto.card.CardBalanceDTO;
-import com.example.bankcards.dto.card.CardResponseDTO;
 import com.example.bankcards.dto.card.CardBalanceResponseDTO;
 import com.example.bankcards.dto.card.CardInfoResponseDTO;
+import com.example.bankcards.security.CustomUserDetails;
 import com.example.bankcards.service.UserCardService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,71 +21,59 @@ public class UserCardController {
 
     private final UserCardService userCardService;
 
-    @GetMapping
-    public ResponseEntity<List<CardResponseDTO>> getUserCards() {
+    @GetMapping("/all")
     public ResponseEntity<List<CardInfoResponseDTO>> getUserCards(
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
         log.info("Resieved request to get all cards");
         List<CardInfoResponseDTO> cards = userCardService.getAllCards(user.getId());
         return ResponseEntity.ok().body(cards);
     }
 
-    @GetMapping(params = {"page","size"})
-    public ResponseEntity<List<CardResponseDTO>> getUserCardsWithPaging(
-            @RequestParam Integer page,
-            @RequestParam Integer size) {
-        log.info("Resieved request to get all cards with page {} and size {}", page, size);
-        List<CardResponseDTO> cards = userCardService.getAllCardsWithPaging(page, size);
-        return ResponseEntity.ok().body(cards);
-    }
-
-    @GetMapping(params = "status")
-    public ResponseEntity<List<CardResponseDTO>> getUserCardsByStatus(@RequestParam String status) {
-        log.info("Received request to get cards with status {}", status);
-        List<CardResponseDTO> cards = userCardService.getAllCardsWithStatus(status);
-        return ResponseEntity.ok().body(cards);
-    }
-
-    @GetMapping(params = {"status","page","size"})
-    public ResponseEntity<List<CardResponseDTO>> getUserCardsByStatusAndPage(
-            @RequestParam String status,
-            @RequestParam Integer page,
-            @RequestParam Integer size) {
+    @GetMapping
     public ResponseEntity<List<CardInfoResponseDTO>> getUserCardsByStatusWithPagination(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam(required = false) String status,
+            @RequestParam @Min(1) int page,
+            @RequestParam @Min(1) int size) {
         log.info("Received request to get cards with status {}, page {} and size {}", status, page, size);
         List<CardInfoResponseDTO> cards = userCardService.getAllCardsWithStatusAndPaging(user.getId(), status, page, size);
         return ResponseEntity.ok().body(cards);
     }
 
-    @GetMapping(params = "pan_last_4")
-    public ResponseEntity<List<CardResponseDTO>> getUserCardsByPan(@RequestParam("pan_last_4") String panLast4) {
+    @GetMapping("/pan")
     public ResponseEntity<CardInfoResponseDTO> getUserCardsByPan(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam("last_4") String panLast4) {
         log.info("Received request to get cards with last 4 digits {}", panLast4);
         CardInfoResponseDTO card = userCardService.getAllCardsByPanLastDigits(user.getId(), panLast4);
         return ResponseEntity.ok().body(card);
     }
 
-    @GetMapping(params = "id")
-    public ResponseEntity<List<CardResponseDTO>> getUserCardsById(@RequestParam Long id) {
-        log.info("Received request to get card with ID {}", id);
-        List<CardResponseDTO> cards = userCardService.getAllCardsById(id);
-        return ResponseEntity.ok().body(cards);
+    @GetMapping("/{cardId}")
     public ResponseEntity<CardInfoResponseDTO> getUserCardsById(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable long cardId) {
+        log.info("Received request to get card with ID {}", cardId);
         CardInfoResponseDTO card = userCardService.getCardsById(user.getId(), cardId);
+        return ResponseEntity.ok().body(card);
     }
 
-    @GetMapping("/{id}/balance")
-    public ResponseEntity<CardBalanceDTO> getCardBalance(@PathVariable Long id) {
-        log.info("Received request to get balance for card ID {}", id);
-        CardBalanceDTO balance = userCardService.getCardBalance(id);
+    @GetMapping("/{cardId}/balance")
     public ResponseEntity<CardBalanceResponseDTO> getCardBalance(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable long cardId) {
+        log.info("Received request to get balance for card ID {}", cardId);
         CardBalanceResponseDTO balance = userCardService.getCardBalance(user.getId(), cardId);
         return ResponseEntity.ok().body(balance);
     }
 
-    @PostMapping("/{id}/block")
-    public ResponseEntity<Void> blockCard(@PathVariable Long id) {
-        log.info("Received request to block card ID {}", id);
-        userCardService.blockCard(id);
+    @PostMapping("/{cardId}/block")
+    public ResponseEntity<Void> blockCard(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable long cardId) {
+        log.info("Received request to block card ID {}", cardId);
+        userCardService.blockCard(user.getId(), cardId);
         return ResponseEntity.ok().build();
     }
 }
