1e3aa370d6d3beddaccb61a73dfdd88611a070ec
paul <kulebyakinpaul@yandex.ru>
2026-04-05 23:52:31 +0300

    Add custom user detail implementation

---
+++ src/main/java/com/example/bankcards/security/CustomUserDetails.java
@@ -0,0 +1,46 @@
+package com.example.bankcards.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class CustomUserDetails implements UserDetails {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
