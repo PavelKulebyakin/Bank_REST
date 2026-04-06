99d1391d191b73c624e3909e663fc4b5520529ef
paul <kulebyakinpaul@yandex.ru>
2026-04-06 17:16:18 +0300

    Add illegal state exception and auth related exceptions handlers

--- src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
@@ -2,10 +2,16 @@ package com.example.bankcards.exception;
 
 import com.example.bankcards.exception.custom.ResourceAlreadyExistsException;
 import com.example.bankcards.exception.custom.ResourceNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,22 +31,40 @@ import java.util.Map;
 @Log4j2
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler({
+            BadCredentialsException.class,
+            UsernameNotFoundException.class,
+            DisabledException.class,
+            LockedException.class
+    })
+    public ResponseEntity<?> handleAuthExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("message", "Invalid credentials"));
+    }
+
+    @ExceptionHandler({
+            ExpiredJwtException.class,
+            SignatureException.class,
+            MalformedJwtException.class
+    })
+    public ResponseEntity<?> handleJwt(Exception ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("message", "Invalid or expired token"));
+    }
+
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Map<String,String>> handleDTOValidationException(MethodArgumentNotValidException ex) {
-        Map<String,String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String filedName = ((FieldError) error).getField();
-            String errorMsg = error.getDefaultMessage();
-            errors.put(filedName, errorMsg);
-        });
-        log.warn("Request validation failed: {}", errors);
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<Map<String, String>> handleRequestVariableValidationException(HandlerMethodValidationException ex) {
+    public ResponseEntity<Map<String, String>> handleParamValidation(HandlerMethodValidationException ex) {
         Map<String, String> errors = new HashMap<>();
-
         ex.getParameterValidationResults().forEach(paramResult ->
                 paramResult.getResolvableErrors().forEach(error -> {
                     if (error instanceof FieldError fieldError) {
@@ -49,60 +74,45 @@ public class GlobalExceptionHandler {
                     }
                 })
         );
-
-        log.warn("Handler method validation failed: {}", errors);
         return ResponseEntity.badRequest().body(errors);
-    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<Map<String, String>> handleJsonParseException(HttpMessageNotReadableException ex) {
-        log.warn(ex.getMessage(), ex);
-        Map<String, String> error = new HashMap<>();
-        error.put("message", "Malformed JSON request");
-        return ResponseEntity.badRequest().body(error);
-    }
-
-    @ExceptionHandler(ResourceAlreadyExistsException.class)
-    protected ResponseEntity<Map<String, String>> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
-        log.warn(ex.getMessage());
-        Map<String, String> error = new HashMap<>();
-        error.put("message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    protected ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        log.warn(ex.getMessage());
-        Map<String, String> error = new HashMap<>();
-        error.put("message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<Map<String, String>> handleInvalidRoleException(IllegalArgumentException ex) {
-        log.warn(ex.getMessage());
-        Map<String, String> error = new HashMap<>();
-        error.put("message", ex.getMessage());
-        return ResponseEntity.badRequest().body(error);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        Map<String, String> errors = new HashMap<>();
+    protected ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
         String name = ex.getName();
-        Object value = ex.getValue();
         String requiredType = ex.getRequiredType() != null ? ex.getRequiredType().getSimpleName() : "Unknown";
-        String message = String.format("Parameter '%s' with value '%s' could not be converted to type %s",
-                name, value, requiredType);
-        errors.put(name, message);
-        return ResponseEntity.badRequest().body(errors);
+        String message = String.format("Parameter '%s' with value '%s' could not be converted to type %s"
+                , name, ex.getValue(), requiredType);
+        return ResponseEntity.badRequest().body(Map.of(name, message));
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<Map<String, String>> handleJsonParse(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(Map.of("message", "Malformed JSON request"));
+    }
+
+
+    @ExceptionHandler({
+            IllegalArgumentException.class,
+            IllegalStateException.class
+    })
+    protected ResponseEntity<Map<String, String>> handleInvalidRole(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    protected ResponseEntity<Map<String, String>> handleResourceAlreadyExists(ResourceAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<Map<String, String>> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", ex.getMessage()));
+    }
+
+
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<Map<String,String>> handleException(Exception ex) {
-        log.error(ex.getMessage(), ex);
-        Map<String,String> errors = new HashMap<>();
-        errors.put("message", "Internal Server Error");
-        return ResponseEntity.internalServerError().body(errors);
+    protected ResponseEntity<Map<String,String>> handle(Exception ex) {
+        return ResponseEntity.internalServerError().body(Map.of("message", "Internal Server Error"));
     }
 }
