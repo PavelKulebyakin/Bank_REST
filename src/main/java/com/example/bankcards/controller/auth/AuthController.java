9e1f0ed38112869c7834084be59c67ab437c81d2
paul <kulebyakinpaul@yandex.ru>
2026-04-06 14:50:39 +0300

    Add auth controller

---
+++ src/main/java/com/example/bankcards/controller/auth/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.bankcards.controller.auth;
+
+import com.example.bankcards.dto.AuthResponseDTO;
+import com.example.bankcards.dto.auth.AuthRequestDTO;
+import com.example.bankcards.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Log4j2
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid AuthRequestDTO request) {
+        log.info("Received request to login with email: {}, password: {}", request.getEmail(), request.getPassword());
+        String token = authService.authenticate(request);
+        return ResponseEntity.ok().body(new AuthResponseDTO(token));
+    }
+}
